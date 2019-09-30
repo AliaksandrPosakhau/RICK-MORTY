7f13d8c373936d955e08a7010b7805143dfafdb7
AliaksandrPosakhau <48470164+AliaksandrPosakhau@users.noreply.github.com>
2019-09-30 19:13:39 +0300

    adding last stable version

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
+const axios = require('axios');
 const yargs = require('yargs');
-const operationFind = require('./OperationFind');
-const startEndpoint = `https://rickandmortyapi.com/api/character/`;
+const fs = require('fs');
+const lodash = require('lodash');
+const firstEndpoint = 'https://rickandmortyapi.com/api/character/';
+const dataFilePath='./responses.json';
 
-// node app find -id=15464 -n="Rick" -s="Alive" -sp="Human" -t="Unknown" -g="Male" -l="Earth"
+// node app find --id=101 --name="E. Coli" --status="Dead" --species="Disease" --type="" --gender="unknown" --location="Anatomy Park"
 
 yargs.command({
     command: 'find',
@@ -39,5 +42,56 @@ yargs.command({
     }
 });
 
-const find = new operationFind(yargs.argv,startEndpoint);
-find.executeOperation();
+getDatabase();
+operationFind(yargs.argv,dataFilePath);
+
+async function getDatabase() {
+    let promisesArray = [];
+    let resultsArray = [];
+    const firstRequestData = await axios.get(firstEndpoint);
+    const amountOfPages = firstRequestData.data.info.pages;
+    resultsArray.push(firstRequestData.data.results);
+
+    for (let i=2;i<amountOfPages;++i){
+        promisesArray.push(axios.get(`https://rickandmortyapi.com/api/character/?page=${i}`));
+    }
+
+    let responses = await Promise.all(promisesArray);
+
+    responses.forEach((item,index)=>{
+       return responses[index]=item.data.results;
+    });
+
+    responses = lodash.flatten(responses);
+    resultsArray = [...resultsArray,... responses];
+    resultsArray = lodash.flatten(resultsArray);
+    return fs.writeFileSync('responses.json',JSON.stringify(resultsArray,null,'\t','utf8'));
+}
+
+async function getReportData(reportPath){
+    return new Promise(resolve => {
+        resolve(fs.readFileSync(dataFilePath));
+    });
+}
+
+async function operationFind(operationsArray, dataFilePath) {
+
+    const currentData = await getReportData(dataFilePath);
+    const jsonParsedData = JSON.parse(currentData.toString());
+
+    let reportArray = jsonParsedData.filter((element) => {
+        return element.name === operationsArray.name &&
+            element.id === operationsArray.id &&
+            element.status === operationsArray.status &&
+            element.species === operationsArray.species &&
+            element.type === operationsArray.type &&
+            element.gender === operationsArray.gender &&
+            element.location.name === operationsArray.location;
+    });
+    return fs.writeFileSync('report.json',JSON.stringify(reportArray,null,'\t','utf8'));
+}
+
+// short java-styled function to operate console log output shorter and more convenient
+function sout(arg){
+    console.log(arg);
+}
